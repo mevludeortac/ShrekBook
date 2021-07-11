dcd8ed94e41730da383f6f7d8b2b80ce5ce8118a
herbin <ortacmevlude@gmail.com>
2021-07-11 21:19:01 +0300

    class

---
+++ ShrekBook/ShrekFam.swift
@@ -0,0 +1,21 @@
+//
+//  ShrekFam.swift
+//  ShrekBook
+//
+//  Created by MEWO on 27.06.2021.
+//
+
+import Foundation
+import UIKit
+class ShrekFam {
+    
+    var name: String
+    var vocalizer : String
+    var image : UIImage
+    
+    init(nameInit:String, vocalizerInit:String, imageInit:UIImage) {
+        name = nameInit
+        vocalizer = vocalizerInit
+        image = imageInit
+    }
+}
